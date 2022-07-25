f5378bc432258681611143d36d434da953785f16
Joel Chigumba <jchigumba@shoprite.co.za>
2022-07-25 11:59:01 +0200

    updated tweet module variables

--- modules/tweets.js
+++ modules/tweets.js
@@ -24,11 +24,11 @@ const readData = async (filename) => {
 
         for await (const line of fileData) {
 
-            let username = line.split('> ')[0];
-            let message = line.split('> ')[1].substring(0, 139);
+            const username = line.split('> ')[0];
             // ensure only 140 characters are stored in each message
+            const message = line.split('> ')[1].substring(0, 139);
 
-            const newTweet = new Tweet({ tweetId, username, message });
+            const newTweet = new Tweet({ tweetId: tweetId++, username, message });
 
             // read each line and populate tweetFeed object map for each user
             if (!tweetFeed[username]) {
@@ -36,8 +36,6 @@ const readData = async (filename) => {
             } else {
                 tweetFeed[username].addTweet(newTweet);
             }
-
-            tweetId++;
         }
 
     } catch (error) {
